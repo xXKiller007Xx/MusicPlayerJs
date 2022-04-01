d6c7fe48ae54925867473b0f69e2d2fc9ecdcc74
xXKiller007Xx <91753795+xXKiller007Xx@users.noreply.github.com>
2022-04-01 16:40:28 +0700

    add list function

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ const audio = $("#aud");
 const range_btn = $("#range_btn");
 const replay_btn = $("#replay_btn");
 const next_btn = $("#next_btn");
+const prev_btn = $("#prev_btn");
 //Declare Var Control
 var index = 0;
 var isPlaying = false;
 var isLoop = false;
-var isShuffle = audio.loop;
+var isShuffle = false;
 //Declare Main Component
 const Music_Player = [
   {
@@ -47,11 +48,11 @@ const Music_Player = [
   },
   {
     id: 3,
-    title: "Em của ngày hôm qua",
-    song_path: "/data/mp3/La Ai Tu Bo, La Ai Vo Tinh.mp3",
-    song_img: "/data/img/La Ai Tu Bo, La Ai Vo Tinh.jpg",
-    singer: "Sơn Tùng M-TP",
-    album: "Em của ngày hôm qua (Single)",
+    title: "Let The Battle Begin! - A Merc's Job",
+    song_path: "/data/mp3/Let The Battles Begin! - A Merc's Job.mp3",
+    song_img: "/data/img/ff7remake.jpg",
+    singer: "",
+    album: "FINAL FANTASY VII REMAKE ORIGINAL SOUNDTRACK",
   },
   {
     id: 4,
@@ -81,25 +82,32 @@ const Music_Player = [
 //Event Handler
 shuffle_btn.addEventListener("click", () => {
   shuffle_btn.classList.toggle("active");
+  isShuffle = !isShuffle;
+  console.log(isShuffle);
 });
 replay_btn.addEventListener("click", () => {
   replay_btn.classList.toggle("active");
   isLoop = !isLoop;
-  window.alert(isLoop);
+  console.log(isLoop);
 });
 audio.addEventListener("ended", () => {
   range_btn.value = 0;
   if (isLoop) {
     audio.loop = isLoop;
     audio.play();
+  }
+  if (isShuffle) {
+    for (let i = 0; i < shuffleMusic(Music_Player).length; i++) {
+      playMusic(shuffleMusic(Music_Player)[i]);
+    }
   } else {
     if (index == Music_Player.length - 1) {
       index = 0;
     } else {
       index++;
     }
+    playMusic(index);
   }
-  playMusic(index);
 });
 play_pause_btn.addEventListener("click", () => {
   if (!isPlaying) {
@@ -119,7 +127,6 @@ play_pause_btn.addEventListener("click", () => {
 audio.addEventListener("timeupdate", () => {
   let playedPercentage = Math.floor((audio.currentTime / audio.duration) * 100);
   range_btn.value = playedPercentage;
-  console.log(playedPercentage);
 });
 range_btn.addEventListener("change", (e) => {
   audio.currentTime = (range_btn.value / 100) * audio.duration;
@@ -127,13 +134,21 @@ range_btn.addEventListener("change", (e) => {
 
 songs.innerHTML = Music_Player.map(
   (e) =>
-    `<li value="${e.id}" onclick="this.classList.add('title_active')"><p>${e.title}</p><p>${e.singer}</p></li>`
+    `<li class="list title_no_active" value="${e.id}"><div><p><b>${e.title}</b></p><p>${e.singer}</p></div></li>`
 ).join("");
 next_btn.addEventListener("click", () => {
   if (index == Music_Player.length - 1) {
     index = 0;
   } else {
     index++;
+  }
+  playMusic(index);
+});
+prev_btn.addEventListener("click", () => {
+  if (index == 0) {
+    index = Music_Player.length - 1;
+  } else {
+    index--;
   }
   playMusic(index);
 });
@@ -151,3 +166,36 @@ function playMusic(index) {
   audio.play();
 }
 set(index);
+
+var shuffleMusic = (arr) => {
+  var tempArr = new Array();
+  tempArr[0] = Math.floor(Math.random() * (arr.length - 1));
+  for (let i = 1; i < arr.length - 1; i++) {
+    tempArr[i] = Math.floor(Math.random() * (arr.length - 1));
+  }
+  return tempArr;
+};
+
+const list = $$(".list");
+function setActive() {
+  list[index].classList.remove("title_no_active");
+  list[index].classList.add("title_active");
+  for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", () => {
+      eraseActive();
+      if (!list[i].classList.contains("title_active")) {
+        list[i].classList.remove("title_no_active");
+        list[i].classList.add("title_active");
+      }
+    });
+  }
+}
+function eraseActive() {
+  for (let i = 0; i < list.length; i++) {
+    list[i].classList.remove("title_active");
+    list[i].classList.add('title_no_active')
+  }
+}
+setActive();
+
+console.log(list);
